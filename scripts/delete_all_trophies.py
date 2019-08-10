3031751d455d7a9bb77c3ff4010bd53140a110f1
jcatoe <catoejr@gmail.com>
2019-08-10 18:21:02 -0400

    Add script to remove trophies from titles

---
+++ scripts/delete_all_trophies.py
@@ -0,0 +1,38 @@
+import requests
+
+from plexapi.server import PlexServer
+
+import settings
+from utils import utils
+
+"""
+Remove all trophies and medals from all movies.
+"""
+
+plex = PlexServer(settings.PLEX_URL, settings.PLEX_TOKEN)
+
+plex_sections = plex.library.sections()
+
+for plex_section in plex_sections:
+
+    if plex_section.type == 'movie':
+        section = plex.library.section(plex_section.title)
+
+        for video in section.all():
+            title = video.title
+
+            trophies = ['🏆', '🥈']
+            if any(trophy in title for trophy in trophies):
+
+                print(f'Removing trophy from "{video.title}"')
+                url = utils.generate_url(params={
+                    'base_url': f'{settings.PLEX_URL}/library/sections/{video.librarySectionID}/all?',
+                    'type': 1,
+                    'id': video.ratingKey,
+                    'includeExternalMedia': 1,
+                    'title.value': title.replace('🏆 ', '').replace('🥈 ', ''),
+                    'title.locked': 1,
+                    'titleSort.locked': 1,
+                    'X-Plex-Token': settings.PLEX_TOKEN
+                })
+                requests.put(url)
