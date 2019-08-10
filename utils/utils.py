00f116adc68074fe029480f93fd061150d6b214e
jcatoe <catoejr@gmail.com>
2019-08-10 18:39:40 -0400

    Add generate_url function

---
+++ utils/utils.py
@@ -0,0 +1,22 @@
+import json
+import os
+import requests
+import urllib.parse
+
+from plexapi.server import PlexServer
+
+import settings
+
+plex = PlexServer(settings.PLEX_URL, settings.PLEX_TOKEN)
+
+
+def generate_url(params):
+    url = params['base_url']
+    for key, value in params.items():
+        if key != 'base_url':
+            url += f'{key}={value}&'
+
+    return url[:-1]
+
+
+
