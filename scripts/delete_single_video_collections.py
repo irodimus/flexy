456f3509be4067bccd726642af111d803df1ee92
jcatoe <catoejr@gmail.com>
2019-08-11 00:53:06 -0400

    Add function to remove single video collections

--- scripts/delete_single_video_collections.py
+++ scripts/delete_single_video_collections.py
@@ -1,1 +1,20 @@
-# pretty straightforward, if setting parameter is True, delete single video collections
+from plexapi.server import PlexServer
+
+import settings
+
+
+def execute():
+    plex = PlexServer(settings.PLEX_URL, settings.PLEX_TOKEN)
+
+    plex_sections = plex.library.sections()
+
+    for plex_section in plex_sections:
+        for collection in plex_section.collection():
+            if collection.childCount <= 1 and collection.title not in settings.IGNORE_SINGLE_VIDEO_COLLECTIONS:
+                print(f'Removing collection: {collection.title}')
+                collection.delete()
+
+
+if __name__ == '__main__':
+    if settings.REMOVE_SINGLE_VIDEO_COLLECTIONS:
+        execute()
