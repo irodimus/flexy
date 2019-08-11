3b561c7cdfc620422607669ba8f4b1db9000fd0f
jcatoe <catoejr@gmail.com>
2019-08-11 00:55:24 -0400

    Add comments

--- scripts/delete_all_collections.py
+++ scripts/delete_all_collections.py
@@ -3,27 +3,29 @@ from plexapi.server import PlexServer
 import settings
 
 
-"""
-Remove all collection tags from all movies and then delete the collection itself.
-You could probably just delete the collection, but I'm not taking any chances.
-"""
+if __name__ == '__main__':
+    """
+    Remove all collection tags from all movies and then delete the collection itself.
+    You could probably just delete the collection, but I'm not taking any chances.
+    """
+    plex = PlexServer(settings.PLEX_URL, settings.PLEX_TOKEN)
 
-plex = PlexServer(settings.PLEX_URL, settings.PLEX_TOKEN)
+    sections = plex.library.sections()
 
-sections = plex.library.sections()
+    for section in sections:
 
-for section in sections:
+        if section.type == 'movie':
+            all_collections = section.collection()
 
-    if section.type == 'movie':
-        all_collections = section.collection()
+            if all_collections:
+                # remove tags from all movies
+                for video in section.all():
+                    print(f'Removing collection tags from video: {video.title}')
+                    video.removeCollection([c.tag for c in video.collections])
 
-        if all_collections:
-            # remove tags from all movies
-            for video in section.all():
-                print(f'Removing collection tags from video: {video.title}')
-                video.removeCollection([c.tag for c in video.collections])
+                # then go through and delete all collections
+                for collection in all_collections:
+                    print(f'Removing collection: {collection.title}')
+                    collection.delete()
 
-            # then go through and delete all collections
-            for collection in all_collections:
-                print(f'Removing collection: {collection.title}')
-                collection.delete()
+        # TODO add process to remove collection tags from tv shows
--- scripts/delete_all_trophies.py
+++ scripts/delete_all_trophies.py
@@ -5,34 +5,35 @@ from plexapi.server import PlexServer
 import settings
 from utils import utils
 
-"""
-Remove all trophies and medals from all movies.
-"""
 
-plex = PlexServer(settings.PLEX_URL, settings.PLEX_TOKEN)
+if __name__ == '__main___':
+    """
+    Remove all trophies and medals from all movies.
+    """
+    plex = PlexServer(settings.PLEX_URL, settings.PLEX_TOKEN)
 
-plex_sections = plex.library.sections()
+    plex_sections = plex.library.sections()
 
-for plex_section in plex_sections:
+    for plex_section in plex_sections:
 
-    if plex_section.type == 'movie':
-        section = plex.library.section(plex_section.title)
+        if plex_section.type == 'movie':
+            section = plex.library.section(plex_section.title)
 
-        for video in section.all():
-            title = video.title
+            for video in section.all():
+                title = video.title
 
-            trophies = ['🏆', '🥈']
-            if any(trophy in title for trophy in trophies):
+                trophies = ['🏆', '🥈']
+                if any(trophy in title for trophy in trophies):
 
-                print(f'Removing trophy from "{title}"')
-                url = utils.generate_url(params={
-                    'base_url': f'{settings.PLEX_URL}/library/sections/{video.librarySectionID}/all?',
-                    'type': 1,
-                    'id': video.ratingKey,
-                    'includeExternalMedia': 1,
-                    'title.value': utils.clean_title(title),
-                    'title.locked': 1,
-                    'titleSort.locked': 1,
-                    'X-Plex-Token': settings.PLEX_TOKEN
-                })
-                requests.put(url)
+                    print(f'Removing trophy from "{title}"')
+                    url = utils.generate_url(params={
+                        'base_url': f'{settings.PLEX_URL}/library/sections/{video.librarySectionID}/all?',
+                        'type': 1,
+                        'id': video.ratingKey,
+                        'includeExternalMedia': 1,
+                        'title.value': utils.clean_title(title),
+                        'title.locked': 1,
+                        'titleSort.locked': 1,
+                        'X-Plex-Token': settings.PLEX_TOKEN
+                    })
+                    requests.put(url)
