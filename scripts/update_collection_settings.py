4ecd0112223f3965c58bb70b8e72ce6e725d85ea
jcatoe <catoejr@gmail.com>
2019-08-11 00:39:52 -0400

    Add comments

--- scripts/update_collection_settings.py
+++ scripts/update_collection_settings.py
@@ -10,10 +10,16 @@ import settings
 from utils import utils
 
 # TODO upload background
-# TODO update description
+# TODO update description (sourced from Trakt?)
 
 
-def upload_poster(collection):
+def upload_poster_via_dropbox(collection):
+    """
+    The Plex API call needs a link to an image so instead of storing them locally, I store them in Dropbox and use the
+    display link generated for each image. To use this, you must have a Dropbox token and have your collection images
+    stored in a folder called "Collections" (else you can change the path for the contents variable below). The image
+    name must match the collection name.
+    """
     dbx = dropbox.Dropbox(settings.DROPBOX_TOKEN)
     contents = dbx.files_list_folder(path='/Collections').entries
 
@@ -38,10 +44,10 @@ def upload_poster(collection):
 
     if image_file_path:
         collection_id = collection.ratingKey
-        print(f'Uploading image for "{collection.title}"')
-
         image_link = dbx.files_get_temporary_link(path=image_file_path).link
 
+        print(f'Uploading image for "{collection.title}"')
+        # need to upload the photo to Plex's database
         upload_poster_url = utils.generate_url(params={
             'base_url': f'{settings.PLEX_URL}/library/metadata/{collection_id}/posters?',
             'url': urllib.parse.quote(image_link),
@@ -49,6 +55,7 @@ def upload_poster(collection):
         })
         requests.post(upload_poster_url)
 
+        # once the photo is in the database, we need to get the url
         get_poster_url = utils.generate_url(params={
             'base_url': f'{settings.PLEX_URL}/library/metadata/{collection_id}/posters?',
             'X-Plex-Token': settings.PLEX_TOKEN
@@ -62,6 +69,7 @@ def upload_poster(collection):
             if rating_key.startswith('upload'):
                 upload_url = rating_key
 
+        # once we have that url, we can set it as the collection image
         update_poster = utils.generate_url(params={
             'base_url': f'{settings.PLEX_URL}/library/metadata/{collection_id}/poster?',
             'url': upload_url,
@@ -102,7 +110,7 @@ def update_collection_mode(collection):
         })
         requests.put(update_settings)
 
-    # hide collection
+    # hide collection as a whole
     if collection.title in settings.ALWAYS_HIDE_COLLECTION:
         print(f'Changing collection mode for "{collection.title}" to "{modes[0]}"')
         update_settings = utils.generate_url(params={
@@ -122,6 +130,7 @@ def update_collection_order(collection):
         1: 'Alphabetical'
     }
 
+    # by release date
     if (count <= settings.SORT_VIDEOS_BY_RELEASE_MAX_COLLECTION_LENGTH) \
             or (settings.SORT_VIDEOS_DEFAULT.lower() == 'release date') \
             or (collection.title in settings.ALWAYS_SORT_BY_RELEASE_DATE):
@@ -133,6 +142,7 @@ def update_collection_order(collection):
         })
         requests.put(update_settings)
 
+    # alphabetically
     if (count > settings.SORT_VIDEOS_BY_RELEASE_MAX_COLLECTION_LENGTH) \
             or (collection.title in settings.ALWAYS_SORT_ALPHABETICALLY):
         print(f'Changing collection order for "{collection.title}" to "{modes[1]}"')
@@ -144,7 +154,7 @@ def update_collection_order(collection):
         requests.put(update_settings)
 
 
-if __name__ == '__main__':
+def execute():
     plex = PlexServer(settings.PLEX_URL, settings.PLEX_TOKEN)
 
     plex_sections = plex.library.sections()
@@ -152,6 +162,12 @@ if __name__ == '__main__':
     for plex_section in plex_sections:
 
         for collection in plex_section.collection():
-            upload_poster(collection=collection)
+            upload_poster_via_dropbox(collection=collection)
             update_collection_mode(collection=collection)
             update_collection_order(collection=collection)
+
+    # TODO add Show processing
+
+
+if __name__ == '__main__':
+    execute()
