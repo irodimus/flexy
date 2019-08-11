fcc4a284bdf412f9da02cae5a64a321104b72c03
jcatoe <catoejr@gmail.com>
2019-08-11 15:18:47 -0400

    Add function to write a list of missing videos by collection

--- scripts/check_for_missing_collection_videos.py
+++ scripts/check_for_missing_collection_videos.py
@@ -1,2 +1,57 @@
+import json
+import os
+import requests
+import urllib.parse
+
+from plexapi.server import PlexServer
+
+import settings
+from utils import utils
+
 # compare Trakt list with Plex collection to find missing videos
 # make sure there's a way to ignore collections, or ignore based on Trakt rating
+
+
+def write_json(data):
+    file_path = os.path.join(settings.ROOT, 'data', 'missing.json')
+    with open(file_path, 'w') as outfile:
+        print(f'Writing to file: {file_path}')
+        json.dump(data, outfile)
+
+
+def execute():
+    """
+    Compare Trakt list with Plex collections to find missing videos.
+    Be warned: this is a long running script.
+    """
+    missing = {}
+
+    plex = PlexServer(settings.PLEX_URL, settings.PLEX_TOKEN)
+
+    sections_by_type = utils.get_sections_by_type(plex=plex)
+
+    for section_title in sections_by_type['movies']:
+        section_config = utils.open_trakt_json('movies')
+        section = plex.library.section(section_title)
+
+        trakt_videos = section_config.keys()
+
+        for trakt_video in trakt_videos:
+            all_collections = section_config[trakt_video].get('collections', None)
+            if all_collections:
+                collections = [collection for collection in all_collections if collection not in settings.IGNORE_MISSING_VIDEOS]
+
+                if collections:
+                    if not any(x for x in section.all() if x.title == trakt_video):
+                        for collection in collections:
+                            print(f'Missing "{trakt_video}" for collection "{collection}"')
+                            try:
+                                missing[collection].append(trakt_video)
+                            except KeyError:
+                                missing[collection] = [trakt_video]
+
+    write_json(data=missing)
+
+
+if __name__ == '__main__':
+    execute()
