f538c942e6a8b9f4d0f412f4ea6d00cca8c3f9a5
jcatoe <catoejr@gmail.com>
2019-08-12 16:56:02 -0400

    Add process to remove collections from TV Shows

--- scripts/delete_all_collections.py
+++ scripts/delete_all_collections.py
@@ -1,12 +1,32 @@
 from plexapi.server import PlexServer
+import requests
+import xml.etree.ElementTree as ET
 
 import settings
+from utils import utils
 
 
-if __name__ == '__main__':
+def get_all_show_collections():
+    section_id = utils.get_type_id(type='show')
+    url = utils.generate_url(params={
+        'base_url': f'{settings.PLEX_URL}/library/sections/{section_id}/all?',
+        'type': utils.get_type_id(type='collection'),
+        'X-Plex-Token': settings.PLEX_TOKEN
+    })
+    r = requests.get(url)
+
+    collections = {}
+    root = ET.fromstring(r.text)
+    for child in root:
+        for element in child.getiterator('Directory'):
+            collections[element.attrib['ratingKey']] = element.attrib['title']
+    return collections
+
+
+def execute():
     """
-    Remove all collection tags from all movies and then delete the collection itself.
-    You could probably just delete the collection, but I'm not taking any chances.
+    Deletes all collections from movie and tv shows.
+
     """
     plex = PlexServer(settings.PLEX_URL, settings.PLEX_TOKEN)
 
@@ -15,17 +35,25 @@ if __name__ == '__main__':
     for section in sections:
 
         if section.type == 'movie':
-            all_collections = section.collection()
+            all_movie_collections = section.collection()
 
-            if all_collections:
-                # remove tags from all movies
-                for video in section.all():
-                    print(f'Removing collection tags from video: {video.title}')
-                    video.removeCollection([c.tag for c in video.collections])
+            if all_movie_collections:
 
-                # then go through and delete all collections
-                for collection in all_collections:
+                for collection in all_movie_collections:
                     print(f'Removing collection: {collection.title}')
                     collection.delete()
 
-        # TODO add process to remove collection tags from tv shows
+        if section.type == 'show':
+            all_show_collections = get_all_show_collections()
+
+            for collection_id, collection_title in all_show_collections.items():
+                print(f'Removing collection: {collection_title}')
+                url = utils.generate_url(params={
+                    'base_url': f'{settings.PLEX_URL}/library/metadata/{collection_id}?',
+                    'X-Plex-Token': settings.PLEX_TOKEN
+                })
+                requests.delete(url)
+
+
+if __name__ == '__main__':
+    execute()
