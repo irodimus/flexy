ca84a010cd83bba00934f3a5b86abfb6603231f9
jcatoe <catoejr@gmail.com>
2019-09-04 22:01:57 -0400

    Add holiday changes

--- scripts/update_collection_settings.py
+++ scripts/update_collection_settings.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime
 import requests
 
 from plexapi.server import PlexServer
@@ -11,46 +12,63 @@ def update_collection_mode(collection):
     count = int(collection.childCount)
 
     modes = {
-        -1: 'Library default',
-        1: 'Hide items in this collection',
-        2: 'Show this collection and its items',
-        0: 'Hide collection'
+        -1: "Library default",
+        1: "Hide items in this collection",
+        2: "Show this collection and its items",
+        0: "Hide collection"
     }
 
     # hide items in collection
     if (count <= settings.HIDE_VIDEOS_MAX_COLLECTION_LENGTH) or (collection.title in settings.IGNORE_COLLECTION_LENGTH):
 
-        print(f'Changing collection mode for "{collection.title}" to "{modes[1]}"')
+        print("Changing collection mode for '{title}' to '{mode}'".format(title=collection.title, mode=modes[1]))
         update_settings = utils.generate_url(params={
-            'base_url': f'{settings.PLEX_URL}/library/metadata/{rating_key}/prefs?',
-            'collectionMode': 1,
-            'X-Plex-Token': settings.PLEX_TOKEN
+            "base_url": "{base_url}/library/metadata/{id}/prefs?".format(base_url=settings.PLEX_URL, id=rating_key),
+            "collectionMode": 1,
+            "X-Plex-Token": settings.PLEX_TOKEN
         })
         requests.put(update_settings)
 
     # show items in collection
     if (collection.title in settings.ALWAYS_SHOW_VIDEOS_IN_COLLECTION) \
-            or (count > settings.HIDE_VIDEOS_MAX_COLLECTION_LENGTH):
+            or (count > settings.HIDE_VIDEOS_MAX_COLLECTION_LENGTH) and (collection.title not in settings.IGNORE_COLLECTION_LENGTH):
 
-        print(f'Changing collection mode for "{collection.title}" to "{modes[2]}"')
+        print("Changing collection mode for '{title}' to '{mode}'".format(title=collection.title, mode=modes[2]))
         update_settings = utils.generate_url(params={
-            'base_url': f'{settings.PLEX_URL}/library/metadata/{rating_key}/prefs?',
-            'collectionMode': 2,
-            'X-Plex-Token': settings.PLEX_TOKEN
+            "base_url": "{base_url}/library/metadata/{id}/prefs?".format(base_url=settings.PLEX_URL, id=rating_key),
+            "collectionMode": 2,
+            "X-Plex-Token": settings.PLEX_TOKEN
         })
         requests.put(update_settings)
 
     # hide collection as a whole
     if collection.title in settings.ALWAYS_HIDE_COLLECTION:
 
-        print(f'Changing collection mode for "{collection.title}" to "{modes[0]}"')
-
+        print("Changing collection mode for '{title}' to '{mode}'".format(title=collection.title, mode=modes[0]))
         update_settings = utils.generate_url(params={
-            'base_url': f'{settings.PLEX_URL}/library/metadata/{rating_key}/prefs?',
-            'collectionMode': 0,
-            'X-Plex-Token': settings.PLEX_TOKEN
+            "base_url": "{base_url}/library/metadata/{id}/prefs?".format(base_url=settings.PLEX_URL, id=rating_key),
+            "collectionMode": 0,
+            "X-Plex-Token": settings.PLEX_TOKEN
         })
         requests.put(update_settings)
+
+    # hide inactive holiday collections
+    if collection.title in settings.HOLIDAY_COLLECTIONS:
+        date_ranges = settings.HOLIDAY_COLLECTIONS[collection.title]
+
+        current_date = datetime.now().date()
+
+        start_date = datetime.strptime(str(current_date.year) + '-' + date_ranges[0], '%Y-%m-%d').date()
+        end_date = datetime.strptime(str(current_date.year) + '-' + date_ranges[1], '%Y-%m-%d').date()
+
+        if not start_date <= current_date <= end_date:
+            print("Changing collection mode for '{title}' to '{mode}'".format(title=collection.title, mode=modes[0]))
+            update_settings = utils.generate_url(params={
+                "base_url": "{base_url}/library/metadata/{id}/prefs?".format(base_url=settings.PLEX_URL, id=rating_key),
+                "collectionMode": 0,
+                "X-Plex-Token": settings.PLEX_TOKEN
+            })
+            requests.put(update_settings)
 
 
 def update_collection_order(collection):
@@ -58,33 +76,31 @@ def update_collection_order(collection):
     count = int(collection.childCount)
 
     modes = {
-        0: 'Release date',
-        1: 'Alphabetical'
+        0: "Release date",
+        1: "Alphabetical"
     }
 
     # by release date
     if (count <= settings.SORT_VIDEOS_BY_RELEASE_MAX_COLLECTION_LENGTH) \
             or (collection.title in settings.ALWAYS_SORT_BY_RELEASE_DATE):
-        print(f'Changing collection order for "{collection.title}" to "{modes[0]}"')
+        print("Changing collection order for '{title}' to '{mode}'".format(title=collection.title, mode=modes[0]))
         update_settings = utils.generate_url(params={
-            'base_url': f'{settings.PLEX_URL}/library/metadata/{rating_key}/prefs?',
-            'collectionSort': 0,
-            'X-Plex-Token': settings.PLEX_TOKEN
+            "base_url": f"{settings.PLEX_URL}/library/metadata/{rating_key}/prefs?",
+            "collectionSort": 0,
+            "X-Plex-Token": settings.PLEX_TOKEN
         })
         requests.put(update_settings)
 
     # alphabetically
     if (count > settings.SORT_VIDEOS_BY_RELEASE_MAX_COLLECTION_LENGTH) \
             or (collection.title in settings.ALWAYS_SORT_ALPHABETICALLY):
-        print(f'Changing collection order for "{collection.title}" to "{modes[1]}"')
+        print("Changing collection order for '{title}' to '{mode}'".format(title=collection.title, mode=modes[1]))
         update_settings = utils.generate_url(params={
-            'base_url': f'{settings.PLEX_URL}/library/metadata/{rating_key}/prefs?',
-            'collectionSort': 1,
-            'X-Plex-Token': settings.PLEX_TOKEN
+            "base_url": f"{settings.PLEX_URL}/library/metadata/{rating_key}/prefs?",
+            "collectionSort": 1,
+            "X-Plex-Token": settings.PLEX_TOKEN
         })
         requests.put(update_settings)
-
-# TODO add holiday-specific collections limitations
 
 
 def execute():
@@ -99,5 +115,5 @@ def execute():
             update_collection_order(collection=collection)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     execute()
