1fb867e4bccecc3a14273d6d2fdf034b46fc2112
jcatoe <catoejr@gmail.com>
2019-09-04 22:28:48 -0400

    Code cleanup

--- scripts/delete_all_collections.py
+++ scripts/delete_all_collections.py
@@ -7,19 +7,19 @@ from utils import utils
 
 
 def get_all_show_collections():
-    section_id = utils.get_type_id(type='show')
+    section_id = utils.get_type_id(type="show")
     url = utils.generate_url(params={
-        'base_url': f'{settings.PLEX_URL}/library/sections/{section_id}/all?',
-        'type': utils.get_type_id(type='collection'),
-        'X-Plex-Token': settings.PLEX_TOKEN
+        "base_url": "{base_url}/library/sections/{id}/all?".format(base_url=settings.PLEX_URL, id=section_id),
+        "type": utils.get_type_id(type="collection"),
+        "X-Plex-Token": settings.PLEX_TOKEN
     })
     r = requests.get(url)
 
     collections = {}
     root = ET.fromstring(r.text)
     for child in root:
-        for element in child.getiterator('Directory'):
-            collections[element.attrib['ratingKey']] = element.attrib['title']
+        for element in child.getiterator("Directory"):
+            collections[element.attrib["ratingKey"]] = element.attrib["title"]
     return collections
 
 
@@ -33,26 +33,26 @@ def execute():
 
     for section in sections:
 
-        if section.type == 'movie':
+        if section.type == "movie":
             all_movie_collections = section.collection()
 
             if all_movie_collections:
 
                 for collection in all_movie_collections:
-                    print(f'Removing collection: {collection.title}')
+                    print(f"Removing collection: {collection.title}")
                     collection.delete()
 
-        if section.type == 'show':
+        if section.type == "show":
             all_show_collections = get_all_show_collections()
 
             for collection_id, collection_title in all_show_collections.items():
-                print(f'Removing collection: {collection_title}')
+                print("Removing collection: {title}".format(title=collection_title))
                 url = utils.generate_url(params={
-                    'base_url': f'{settings.PLEX_URL}/library/metadata/{collection_id}?',
-                    'X-Plex-Token': settings.PLEX_TOKEN
+                    "base_url": "{base_url}/library/metadata/{id}?".format(base_url=settings.PLEX_URL, id=collection_id),
+                    "X-Plex-Token": settings.PLEX_TOKEN
                 })
                 requests.delete(url)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     execute()
--- scripts/delete_all_trophies.py
+++ scripts/delete_all_trophies.py
@@ -5,8 +5,7 @@ from plexapi.server import PlexServer
 import settings
 from utils import utils
 
-
-if __name__ == '__main___':
+if __name__ == "__main___":
     """
     Remove all trophies and medals from all movies.
     """
@@ -16,24 +15,24 @@ if __name__ == '__main___':
 
     for plex_section in plex_sections:
 
-        if plex_section.type == 'movie':
+        if plex_section.type == "movie":
             section = plex.library.section(plex_section.title)
 
             for video in section.all():
                 title = video.title
 
-                trophies = ['🏆', '🥈']
+                trophies = ["🏆", "🥈"]
                 if any(trophy in title for trophy in trophies):
-
-                    print(f'Removing trophy from "{title}"')
+                    print("Removing trophy from '{title}'".format(title=title))
                     url = utils.generate_url(params={
-                        'base_url': f'{settings.PLEX_URL}/library/sections/{video.librarySectionID}/all?',
-                        'type': 1,
-                        'id': video.ratingKey,
-                        'includeExternalMedia': 1,
-                        'title.value': utils.clean_title(title),
-                        'title.locked': 1,
-                        'titleSort.locked': 1,
-                        'X-Plex-Token': settings.PLEX_TOKEN
+                        "base_url": "{base_url}/library/sections/{id}/all?".format(base_url=settings.PLEX_URL,
+                                                                                   id=video.librarySectionID),
+                        "type": 1,
+                        "id": video.ratingKey,
+                        "includeExternalMedia": 1,
+                        "title.value": utils.clean_title(title),
+                        "title.locked": 1,
+                        "titleSort.locked": 1,
+                        "X-Plex-Token": settings.PLEX_TOKEN
                     })
                     requests.put(url)
